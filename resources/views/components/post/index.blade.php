d8fd67feacf3aa78da9cbdb26ad408c2535e8cf4
bernard chisumo <bchisumo74@yahoo.com>
2024-07-31 23:03:28 +0100

    Components Slots

--- resources/views/components/post/index.blade.php
+++ resources/views/components/post/index.blade.php
@@ -2,8 +2,8 @@
 <div class="col-md-4 mb-2">
     <div class="card">
         <div class="card-body">
-            <h4>{{ $post->title }}</h4>
-            <h4>{{ $post->description }}</h4>
+            <h4>{{ $title }}</h4>
+            <h4>{{ $description }}</h4>
         </div>
     </div>
 </div>
